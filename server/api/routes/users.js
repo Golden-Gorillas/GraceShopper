da4badd3f2367a6046cbdb45ac134e47625d7cda
CJ Fung <cjfung94@gmail.com>
2021-11-09 22:10:44 +0000

    Changed to userEmail

--- server/api/routes/users.js
+++ server/api/routes/users.js
@@ -1,31 +1,45 @@
 const router = require('express').Router();
 const {
-  models: { User, Cart },
+	models: { User, Cart },
 } = require('../../db');
 
-router.get('/', async (req, res, next) => {
-  try {
-    const users = await User.findAll({
-      include: {
-        model: Cart,
-      },
-    });
-    res.json(users);
-  } catch (err) {
-    next(err);
-  }
+const requireToken = async (req, res, next) => {
+	try {
+		const token = req.headers.authorization;
+		const user = await User.findByToken(token);
+		req.user = user;
+		next();
+	} catch (error) {
+		next(error);
+	}
+};
+
+router.get('/', requireToken, async (req, res, next) => {
+	try {
+		console.log(requireToken);
+
+		const users = await User.findAll({
+			include: {
+				model: Cart,
+			},
+		});
+		res.json(users);
+	} catch (err) {
+		next(err);
+	}
 });
 
-router.get('/:id', async (req, res, next) => {
-  try {
-    const user = await User.findOne({
-      where: { id: req.params.id },
-      include: { model: Cart },
-    });
-    res.send(user);
-  } catch (err) {
-    next(err);
-  }
+router.get('/:id', requireToken, async (req, res, next) => {
+	try {
+		const user = await User.findOne({
+			where: { id: req.params.id },
+			include: { model: Cart },
+		});
+
+		res.send(user);
+	} catch (err) {
+		next(err);
+	}
 });
 
 module.exports = router;
