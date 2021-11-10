0e01121b301d278b432c7a73d863bdbdeca00055
violettebunny <52981973+violettebunny@users.noreply.github.com>
2021-11-09 20:37:38 -0500

    temporary fix for state in pokemoncardInvetory

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,89 +1,75 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const dummyData =[
+const dummyData = [
   {
     id: 1,
-    name: "dummyCHARIZARD - 4/102 - HOLO RARE (CLASSIC COLLECTION)",
-    price: 160.00,
-    description: "this card is in mint condition and very rare",
-    imageUrl: "https://cdn.shopify.com/s/files/1/1704/1809/products/27_19cc09e1-f0dc-4a7d-b87c-6049d6b5896b_324x.jpg?v=1633532772",
+    name: 'dummyCHARIZARD - 4/102 - HOLO RARE (CLASSIC COLLECTION)',
+    price: 160.0,
+    description: 'this card is in mint condition and very rare',
+    imageUrl:
+      'https://cdn.shopify.com/s/files/1/1704/1809/products/27_19cc09e1-f0dc-4a7d-b87c-6049d6b5896b_324x.jpg?v=1633532772',
     inStock: true,
-    rarity: "rare",
-    type: "fire",
+    rarity: 'rare',
+    type: 'fire',
   },
   {
     id: 2,
-    name: "dummyBLASTOISE - 2/102 - HOLO RARE (CLASSIC COLLECTION)",
-    price: 20.00,
-    description: "this card is in good condition and rare",
-    imageUrl: "https://cdn.shopify.com/s/files/1/1704/1809/products/26_6c312927-2d55-4614-a78e-b5e77bfb1e0b_324x.jpg?v=1633532745",
+    name: 'dummyBLASTOISE - 2/102 - HOLO RARE (CLASSIC COLLECTION)',
+    price: 20.0,
+    description: 'this card is in good condition and rare',
+    imageUrl:
+      'https://cdn.shopify.com/s/files/1/1704/1809/products/26_6c312927-2d55-4614-a78e-b5e77bfb1e0b_324x.jpg?v=1633532745',
     inStock: true,
-    rarity: "rare",
-    type: "water",
+    rarity: 'rare',
+    type: 'water',
   },
   {
     id: 3,
-    name: "dummyCLAYDOL - 15/106 - HOLO RARE (CLASSIC COLLECTION)",
-    price: 20.00,
-    description: "this card is in good condition and rare",
-    imageUrl: "https://cdn.shopify.com/s/files/1/1704/1809/products/41_829ce7ed-5f8d-48b5-851d-531c7d4da22f_325x.jpg?v=1633532856",
+    name: 'dummyCLAYDOL - 15/106 - HOLO RARE (CLASSIC COLLECTION)',
+    price: 20.0,
+    description: 'this card is in good condition and rare',
+    imageUrl:
+      'https://cdn.shopify.com/s/files/1/1704/1809/products/41_829ce7ed-5f8d-48b5-851d-531c7d4da22f_325x.jpg?v=1633532856',
     inStock: true,
-    rarity: "rare",
-    type: "water",
-  }
-]
+    rarity: 'rare',
+    type: 'water',
+  },
+];
 /**
  * COMPONENT
  */
-export const Home = props => {
-
-
-
-    const cards = dummyData
-    return (
-      <div>
-        <div className="banner">
-      <div className= "bannerText">
-        <h1>Golden Gorillas</h1>
-        <h1>Pokemon Card Store</h1>
-      </div>
-          {/* <img className="banner" src="http://frugalfindsduringnaptime.com/wp-content/uploads/2019/07/pokemon-channel.jpg"/> */}
-
+export const Home = (props) => {
+  const cards = dummyData;
+  return (
+    <div>
+      <div className="banner">
+        <div className="bannerText">
+          <h1>Golden Gorillas</h1>
+          <h1>Pokemon Card Store</h1>
         </div>
-        <div className="cardsContainer">
+        {/* <img className="banner" src="http://frugalfindsduringnaptime.com/wp-content/uploads/2019/07/pokemon-channel.jpg"/> */}
+      </div>
+      <div className="cardsContainer">
         {cards.map((card) => (
-          <div className= "cards" key = {card.id}>
-            <img src= {card.imageUrl}/>
+          <div className="cards" key={card.id}>
+            <img src={card.imageUrl} />
             <h6>{card.name}</h6>
             <p>${card.price}</p>
           </div>
         ))}
-        </div>
       </div>
-    )
-
-
-
-
-}
-export const Home = (props) => {
-	const { userEmail } = props;
-
-	return (
-		<div>
-			<h3>Welcome, {userEmail}</h3>
-		</div>
-	);
+    </div>
+  );
 };
 
 /**
  * CONTAINER
  */
 const mapState = (state) => {
-	return {
-		userEmail: state.auth.userEmail,
-	};
+  return {
+    userEmail: state.auth.userEmail,
+  };
 };
 
 export default connect(mapState)(Home);
--- client/store/index.js
+++ client/store/index.js
@@ -1,21 +1,44 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import {createLogger} from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import auth from './auth'
-import cardsReducer from './pokemoncards'
-import cardReducer from './pokemoncard'
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import auth from './auth';
+import cardsReducer from './pokemoncards';
+import cardReducer from './pokemoncard';
+import axios from 'axios';
 //auth,
-const reducer = combineReducers(
-  { auth,
-    cardsReducer,
-    cardReducer
- 
-  })
-const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
-)
-const store = createStore(reducer, middleware)
 
-export default store
-export * from './auth'
+let middleware = [
+  // `withExtraArgument` gives us access to axios in our async action creators!
+  // https://github.com/reduxjs/redux-thunk#injecting-a-custom-argument
+  thunkMiddleware.withExtraArgument({ axios }),
+];
+
+// We'd like the redux logger to only log messages when it's running in the
+// browser, and not when we run the tests from within Mocha.
+middleware = [...middleware, createLogger({ collapsed: true })];
+
+const appReducer = combineReducers({
+  cards: cardsReducer,
+  card: cardReducer,
+  auth,
+});
+
+const RESET_STORE = 'RESET_STORE';
+export const resetStore = () => ({ type: RESET_STORE });
+const rootReducer = (state, action) => {
+  if (action.type === RESET_STORE) {
+    state = undefined;
+    return appReducer(state, action);
+  }
+  return appReducer(state, action);
+};
+
+export default createStore(
+  rootReducer,
+  // 👇 This uses the Redux DevTools extension, assuming you have it installed in your browser.
+  // 👇 See: https://github.com/zalmoxisus/redux-devtools-extension
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export * from './auth';
