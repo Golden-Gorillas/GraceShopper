de41f1450701b10fa3a6367b304bee2861f35068
CJ Fung <cjfung94@gmail.com>
2021-11-10 16:51:52 +0000

    Updated Steven Changes

--- client/store/index.js
+++ client/store/index.js
@@ -7,24 +7,11 @@ import cardsReducer from './pokemoncards';
 import cardReducer from './pokemoncard';
 import axios from 'axios';
 //auth,
-<<<<<<< HEAD
-=======
-const reducer = combineReducers(
-  { auth,
-    cards:cardsReducer,
-    card:cardReducer
- 
-  })
-const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
-)
-const store = createStore(reducer, middleware)
->>>>>>> ae86665877566e63bcd39048a3ee9d8b4cfa5704
 
 let middleware = [
-  // `withExtraArgument` gives us access to axios in our async action creators!
-  // https://github.com/reduxjs/redux-thunk#injecting-a-custom-argument
-  thunkMiddleware.withExtraArgument({ axios }),
+	// `withExtraArgument` gives us access to axios in our async action creators!
+	// https://github.com/reduxjs/redux-thunk#injecting-a-custom-argument
+	thunkMiddleware.withExtraArgument({ axios }),
 ];
 
 // We'd like the redux logger to only log messages when it's running in the
@@ -32,26 +19,26 @@ let middleware = [
 middleware = [...middleware, createLogger({ collapsed: true })];
 
 const appReducer = combineReducers({
-  cards: cardsReducer,
-  card: cardReducer,
-  auth,
+	cards: cardsReducer,
+	card: cardReducer,
+	auth,
 });
 
 const RESET_STORE = 'RESET_STORE';
 export const resetStore = () => ({ type: RESET_STORE });
 const rootReducer = (state, action) => {
-  if (action.type === RESET_STORE) {
-    state = undefined;
-    return appReducer(state, action);
-  }
-  return appReducer(state, action);
+	if (action.type === RESET_STORE) {
+		state = undefined;
+		return appReducer(state, action);
+	}
+	return appReducer(state, action);
 };
 
 export default createStore(
-  rootReducer,
-  // 👇 This uses the Redux DevTools extension, assuming you have it installed in your browser.
-  // 👇 See: https://github.com/zalmoxisus/redux-devtools-extension
-  composeWithDevTools(applyMiddleware(...middleware))
+	rootReducer,
+	// 👇 This uses the Redux DevTools extension, assuming you have it installed in your browser.
+	// 👇 See: https://github.com/zalmoxisus/redux-devtools-extension
+	composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export * from './auth';
--- client/store/pokemoncards.js
+++ client/store/pokemoncards.js
@@ -1,39 +1,32 @@
 import axios from 'axios';
 //action
-<<<<<<< HEAD
-const SET_CARDS = 'SET_CARDS';
-=======
-export const SET_CARDS = 'SET_CARDS'
->>>>>>> ae86665877566e63bcd39048a3ee9d8b4cfa5704
+export const SET_CARDS = 'SET_CARDS';
 
 export const setCards = (cards) => {
-  return {
-    type: SET_CARDS,
-    cards,
-  };
+	return {
+		type: SET_CARDS,
+		cards,
+	};
 };
 //thunk
 export const fetchCards = () => {
-<<<<<<< HEAD
-  return async (dispatch) => {
-    const { data } = await axios.get('/api/cards');
-    dispatch(setCards(data));
-  };
+	return async (dispatch) => {
+		try {
+			const { data: cards } = await axios.get('/api/cards');
+			//console.log("THUNK:")
+			dispatch(setCards(cards));
+		} catch (error) {
+			console.log(error);
+		}
+	};
 };
-=======
-    return async (dispatch) => {
-        try{
-            const {data: cards} = await axios.get('/api/cards')
-            //console.log("THUNK:")
-            dispatch(setCards(cards))
->>>>>>> ae86665877566e63bcd39048a3ee9d8b4cfa5704
 
 //reduce
 export default function cardsReducer(state = [], action) {
-  switch (action.type) {
-    case SET_CARDS:
-      return action.cards;
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case SET_CARDS:
+			return action.cards;
+		default:
+			return state;
+	}
 }
