c4b3343f8b9dcd3ee75204d253508c45b74e3526
CJ Fung <cjfung94@gmail.com>
2021-11-10 17:58:27 +0000

    Added UserCart - Unauthorized cart id

--- client/Routes.js
+++ client/Routes.js
@@ -6,6 +6,8 @@ import Home from './components/Home';
 import PokemonCards from './components/PokemonCardInventory';
 import SingleCardView from './components/singleCardView';
 import { me } from './store';
+import UserCart from './components/UserCart';
+
 /**
  * COMPONENT
  */
@@ -23,6 +25,7 @@ class Routes extends Component {
 					<Route exact path='/signup' component={Signup} />
 					<Route exact path='/cards' component={PokemonCards} />
 					<Route exact path='/cards/:id' component={SingleCardView} />
+					<Route exact path='/cart' component={UserCart} />
 				</Switch>
 			</div>
 		);
