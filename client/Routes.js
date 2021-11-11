6ae10a7cea637ff4145f42cc5e0b3e9495180ac0
violettebunny <52981973+violettebunny@users.noreply.github.com>
2021-11-11 16:05:31 -0500

    new routes added for admin page to fetchs, added admin page, and created redux reducer for users

--- client/Routes.js
+++ client/Routes.js
@@ -7,48 +7,50 @@ import PokemonCards from './components/PokemonCardInventory';
 import SingleCardView from './components/singleCardView';
 import { me } from './store';
 import UserCart from './components/UserCart';
+import Admin from './components/Admin';
 
 /**
  * COMPONENT
  */
 class Routes extends Component {
-	async componentDidMount() {
-		this.props.loadInitialData();
-	}
+  async componentDidMount() {
+    this.props.loadInitialData();
+  }
 
-	render() {
-		return (
-			<div>
-				<Switch>
-					<Route exact path='/' component={Home} />
-					<Route exact path='/login' component={Login} />
-					<Route exact path='/signup' component={Signup} />
-					<Route exact path='/cards' component={PokemonCards} />
-					<Route exact path='/cards/:id' component={SingleCardView} />
-					<Route exact path='/cart' component={UserCart} />
-				</Switch>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/cards" component={PokemonCards} />
+          <Route exact path="/cards/:id" component={SingleCardView} />
+          <Route exact path="/cart" component={UserCart} />
+          <Route exact path="/admin" component={Admin} />
+        </Switch>
+      </div>
+    );
+  }
 }
 /**
  * CONTAINER
  */
 const mapState = (state) => {
-	return {
-		// Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
-		// Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
-		isLoggedIn: !!state.auth.id,
-		cards: state.cards,
-	};
+  return {
+    // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
+    // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
+    isLoggedIn: !!state.auth.id,
+    cards: state.cards,
+  };
 };
 
 const mapDispatch = (dispatch) => {
-	return {
-		loadInitialData() {
-			dispatch(me());
-		},
-	};
+  return {
+    loadInitialData() {
+      dispatch(me());
+    },
+  };
 };
 
 export default connect(mapState, mapDispatch)(Routes);
