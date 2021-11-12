7635c30e7ec3f705337acb6a0dc1524b873aa729
violettebunny <52981973+violettebunny@users.noreply.github.com>
2021-11-12 13:35:43 -0500

    have a working API route & thunk to update quantity

--- client/components/UserCart.js
+++ client/components/UserCart.js
@@ -1,57 +1,59 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchCart, removeSpecifiedCard } from '../store/usercart';
+import { fetchCart, removeSpecifiedCard, setQuantity } from '../store/usercart';
 
 export class UserCart extends React.Component {
-	componentDidMount() {
-		let cartId = window.localStorage.getItem('cartId');
-		if (!cartId || cartId === undefined) {
-			cartId = JSON.stringify(this.props.id.cart.id);
-			window.localStorage.setItem('cartId', cartId);
-		}
-		this.props.loadCart(JSON.parse(cartId));
-	}
+  componentDidMount() {
+    let cartId = window.localStorage.getItem('cartId');
+    if (!cartId || cartId === undefined) {
+      cartId = JSON.stringify(this.props.id.cart.id);
+      window.localStorage.setItem('cartId', cartId);
+    }
+    this.props.loadCart(JSON.parse(cartId));
+  }
 
-	render() {
-		const { cart, deleteCard } = this.props;
-		return (
-			<div>
-				{!cart.cards
-					? 'No Data'
-					: cart.cards.map((card) => {
-							return (
-								<div key={card.id}>
-									<div>
-										<div>
-											<img className='cartImage' src={card.imageUrl} />
-										</div>
-										<div>{card.name}</div>
-										<div>{card.price}</div>
-										<div>{card.stock}</div>
-									</div>
-									<button onClick={() => deleteCard(cart.id, card.id)}>
-										X
-									</button>
-								</div>
-							);
-					  })}
-			</div>
-		);
-	}
+  render() {
+    const { cart, deleteCard } = this.props;
+    return (
+      <div>
+        {!cart.cards
+          ? 'No Data'
+          : cart.cards.map((card) => {
+              return (
+                <div key={card.id}>
+                  <div>
+                    <div>
+                      <img className="cartImage" src={card.imageUrl} />
+                    </div>
+                    <div>{card.name}</div>
+                    <div>{card.price}</div>
+                    <div>{card.stock}</div>
+                  </div>
+                  <button onClick={() => deleteCard(cart.id, card.id)}>
+                    X
+                  </button>
+                </div>
+              );
+            })}
+      </div>
+    );
+  }
 }
 
 const mapState = (state) => {
-	return {
-		cart: state.cart,
-		id: state.auth,
-	};
+  return {
+    cart: state.cart,
+    id: state.auth,
+  };
 };
 
 const mapDispatch = (dispatch, { history }) => {
-	return {
-		loadCart: (id) => dispatch(fetchCart(id)),
-		deleteCard: (cartId, card) => dispatch(removeSpecifiedCard(cartId, card)),
-	};
+  return {
+    loadCart: (id) => dispatch(fetchCart(id)),
+    deleteCard: (cartId, card) => dispatch(removeSpecifiedCard(cartId, card)),
+    updateQuantity: (cartId, cardId, quantity) =>
+      dispatch(setQuantity(cartId, cardId, quantity)),
+  };
 };
 
 export default connect(mapState, mapDispatch)(UserCart);
--- client/store/usercart.js
+++ client/store/usercart.js
@@ -3,74 +3,103 @@ import axios from 'axios';
 const SET_CART = 'SET_CART';
 const REMOVE_CARD = 'REMOVE_CARD';
 const ADD_TO_CART = 'ADD_TO_CART';
+const SET_QUANTITY = 'SET_QUANTITY';
 
 const setCart = (cart) => {
-	return {
-		type: SET_CART,
-		cart,
-	};
+  return {
+    type: SET_CART,
+    cart,
+  };
 };
 
 const removeCard = (cart) => {
-	return {
-		type: REMOVE_CARD,
-		cart,
-	};
+  return {
+    type: REMOVE_CARD,
+    cart,
+  };
 };
 
 const addToCart = (cart) => {
-	return {
-		type: ADD_TO_CART,
-		cart,
-	};
+  return {
+    type: ADD_TO_CART,
+    cart,
+  };
+};
+
+const _setQuantity = (cart) => {
+  return {
+    type: SET_QUANTITY,
+    cart,
+  };
+};
+
+// export const updateCard = (card, history) => {
+//   return async (dispatch) => {
+//     const token = window.localStorage.getItem(TOKEN);
+//     const { data: updated } = await axios.put(`/api/cards/${card.id}`, card, {
+//       headers: { authorization: token },
+//     });
+//     dispatch(_updateCard(updated));
+//     history.push(`/cards/${card.id}`);
+//   };
+// };
+
+export const setQuantity = (cartId, cardId, quantity) => {
+  return async (dispatch) => {
+    console.log('From the redux store:', cartId, cardId, quantity);
+    const { data } = await axios.put(`/api/cardInCarts`, {
+      data: { cartId, cardId, quantity },
+    });
+    dispatch(_setQuantity(data));
+  };
 };
 
 export const fetchCart = (id) => {
-	return async (dispatch) => {
-		try {
-			const { data } = await axios.get(`/api/carts/${id}`);
-			dispatch(setCart(data));
-		} catch (error) {
-			console.error(error);
-		}
-	};
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`/api/carts/${id}`);
+      dispatch(setCart(data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
 };
 
 export const removeSpecifiedCard = (cartId, cardId) => {
-	return async (dispatch) => {
-		try {
-			const { data } = await axios.put(`/api/carts/${cartId}`, {
-				delete: cardId,
-			});
-			dispatch(removeCard(data));
-		} catch (error) {
-			console.error(error);
-		}
-	};
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.put(`/api/carts/${cartId}`, {
+        delete: cardId,
+      });
+      dispatch(removeCard(data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
 };
 
 export const addCardToCart = (cartId, cardId) => {
-	return async (dispatch) => {
-		try {
-			const { data } = await axios.put(`/api/carts/${cartId}`, {
-				add: cardId,
-			});
-			dispatch(addToCart(data));
-		} catch (error) {
-			console.error(error);
-		}
-	};
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.put(`/api/carts/${cartId}`, {
+        add: cardId,
+      });
+      dispatch(addToCart(data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
 };
 
 export default function cartReducer(state = [], action) {
-	switch (action.type) {
-		case SET_CART:
-			return action.cart;
-		case REMOVE_CARD:
-			return action.cart;
-		case ADD_TO_CART:
-			return action.cart;
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case SET_CART:
+      return action.cart;
+    case REMOVE_CARD:
+      return action.cart;
+    case ADD_TO_CART:
+      return action.cart;
+    default:
+      return state;
+  }
 }
