a38cdc62276822d7729c9e6ba441749ee83b5222
Steven <shotta3xd@gmail.com>
2021-11-11 21:14:53 -0500

    updated css

--- client/components/PokemonCardInventory.js
+++ client/components/PokemonCardInventory.js
@@ -9,6 +9,9 @@ import { fetchCards } from '../store/pokemoncards';
 export class PokemonCards extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			test: []
+		}
 	}
 	componentDidMount() {
 		this.props.getCards();
@@ -16,17 +19,23 @@ export class PokemonCards extends Component {
 	render() {
 		const { cards = [] } = this.props;
 		console.log(this.props);
-
+		//const sorted = [...cards].sort()
 		return (
 			<div>
+				<select>
+					<option>Lowest->Highest</option>
+					<option>Highest->Lowest</option>
+					<option>A-Z</option>
+				</select>
 				<h1>card list</h1>
 				<div className='cardsContainer'>
 					{cards.map((card) => (
 						<div className='singleRowContainer' key={card.id}>
-							<div>
-								<Link to={`/cards/${card.id}`} ><h1>{card.name}</h1></Link>
-							</div>	
-							<p className="price">{card.price}</p>
+							
+							<Link className="namelink" to={`/cards/${card.id}`} ><h1 id="cardname">{card.name}</h1></Link>
+							
+							
+							<p className="price">$ {card.price}</p>
 							<img className="card" src={card.imageUrl} />
 						</div>
 					))}
