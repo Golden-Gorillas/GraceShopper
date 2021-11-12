7adcdb369774ef7d2d88c762c0ff7e2faa41db9b
CJ Fung <cjfung94@gmail.com>
2021-11-12 20:50:15 +0000

    Updated merge

--- client/components/UserCart.js
+++ client/components/UserCart.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { fetchCart, removeSpecifiedCard, setQuantity } from '../store/usercart';
 
 export class UserCart extends React.Component {
+<<<<<<< Updated upstream
   componentDidMount() {
     let cartId = window.localStorage.getItem('cartId');
     if (!cartId || cartId === undefined) {
@@ -67,6 +68,63 @@ export class UserCart extends React.Component {
       </div>
     );
   }
+=======
+	componentDidMount() {
+		//Guest Token
+		//Guest Cart
+
+		let cartId = window.localStorage.getItem('cartId');
+		// if (!cartId || cartId === undefined) {
+		if (this.props.id === {}) {
+			//If we have don't have a props.id => Guest
+			//Create a cartId Token and save it to the localStorage
+
+			cartId = JSON.stringify(this.props.id.cart.id);
+			// userId = JSON.stringify(this.props.id.id);
+			window.localStorage.setItem('cartId', cartId);
+		}
+
+		this.props.loadCart(JSON.parse(cartId));
+	}
+
+	render() {
+		const { cart, deleteCard } = this.props;
+		return (
+			<div>
+				{!cart.cards
+					? 'No Data'
+					: cart.cards.map((card) => {
+							return (
+								<div key={card.id}>
+									<div>
+										<div>
+											<img className='cartImage' src={card.imageUrl} />
+										</div>
+										<div>{card.name}</div>
+										<div>{card.price}</div>
+										{console.log(cart)}
+										<div>{card.stock}</div>
+
+										{/* <label for="Quantity">Quantity</label> */}
+										<input
+											type='number'
+											id='quantity'
+											min='1'
+											max={card.stock}
+										/>
+										<input type='submit' />
+									</div>
+									<button onClick={() => deleteCard(cart.id, card.id)}>
+										X
+									</button>
+								</div>
+							);
+					  })}
+				<button type='button'>CHECKOUT</button>
+			</div>
+		);
+	}
+>>>>>>> Stashed changes
 }
 
 const mapState = (state) => {
