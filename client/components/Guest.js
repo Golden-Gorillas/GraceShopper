5c4d62a5fe258398f2cf88c1aba32885bf19212a
Lydia Rees <lydiairis76@gmail.com>
2021-11-14 16:37:08 -0600

    added users view on admin page, changed home page css

--- client/components/Guest.js
+++ client/components/Guest.js
@@ -1,2 +1,19 @@
-// AddCardToGuestCart Function (cart, card)
-// RemoveCardFromGuestCart Function (cart,card)
+// // AddCardToGuestCart Function (cart, card)
+// const AddCardToGuestCart = (id, cart ) => {
+//   window.localStorage.setItem(id)
+// }
+
+// // RemoveCardFromGuestCart Function (cart,card)
+// const RemoveCardFromGuestCart = (id, cart) => {
+//   window.localStorage.removeItem(id)
+// }
+
+// const cartObj = {
+//   item1: {},
+//   item2: {},
+// }
+
+// localStorage.setItem("cart", JSON.stringify(cartObj));
+
+// // getting back the object
+// const copyCartObj = JSON.parse(localStorage.getItem("cart"));
