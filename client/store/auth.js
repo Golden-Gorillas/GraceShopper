5f733d9dc764b0c3c91ac472f2204a2a400c4806
CJ Fung <cjfung94@gmail.com>
2021-11-15 19:57:17 +0000

    Completed Merges

--- client/store/auth.js
+++ client/store/auth.js
@@ -18,62 +18,47 @@ const setAuth = (auth) => ({ type: SET_AUTH, auth });
  * THUNK CREATORS
  */
 export const me = () => async (dispatch) => {
-  const token = window.localStorage.getItem(TOKEN);
-  if (token) {
-    const res = await axios.get('/auth/me', {
-      headers: {
-        authorization: token,
-      },
-    });
-    return dispatch(setAuth(res.data));
-  }
+	const token = window.localStorage.getItem(TOKEN);
+	if (token) {
+		const res = await axios.get('/auth/me', {
+			headers: {
+				authorization: token,
+			},
+		});
+		return dispatch(setAuth(res.data));
+	}
 };
 
 export const authenticate =
-<<<<<<< HEAD
-  (userEmail, password, method, history) => async (dispatch) => {
-    try {
-      const res = await axios.post(`/auth/${method}`, { userEmail, password });
-      window.localStorage.setItem(TOKEN, res.data.token);
-      dispatch(me());
-      history.push('/');
-    } catch (authError) {
-      return dispatch(setAuth({ error: authError }));
-    }
-  };
-=======
-	(userEmail, password, method) => async (dispatch) => {
+	(userEmail, password, method, history) => async (dispatch) => {
 		try {
 			const res = await axios.post(`/auth/${method}`, { userEmail, password });
 			window.localStorage.setItem(TOKEN, res.data.token);
-			//guestCart = window.localStorage.getItem('guestCart');
-			// Cart => axios.put with guestCart
-			//window.localStorage.removeItem('guestCart');
 			dispatch(me());
+			history.push('/');
 		} catch (authError) {
 			return dispatch(setAuth({ error: authError }));
 		}
 	};
->>>>>>> 054f886a43ffeb52ec7f67ce787b62a0405cdac6
 
 export const logout = () => {
-  window.localStorage.removeItem(TOKEN);
-  window.localStorage.removeItem(CARTID);
-  history.push('/login');
-  return {
-    type: SET_AUTH,
-    auth: {},
-  };
+	window.localStorage.removeItem(TOKEN);
+	window.localStorage.removeItem(CARTID);
+	history.push('/login');
+	return {
+		type: SET_AUTH,
+		auth: {},
+	};
 };
 
 /**
  * REDUCER
  */
 export default function (state = {}, action) {
-  switch (action.type) {
-    case SET_AUTH:
-      return action.auth;
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case SET_AUTH:
+			return action.auth;
+		default:
+			return state;
+	}
 }
